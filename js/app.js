4d57427a29a2cbb216113945fb9bd940d43681bc
curriedav <curriedav@gmail.com>
2013-12-03 19:45:55 -0800

    adds AppView

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(function () {
+
+	//Creates a view
+	var AppView = Backbone.View.extend({
+		//pass an object with the particalurs that describes what an app view is
+		//every app view has to have an associated DOM element
+		el: 'body', //binds changes to backbone to the DOM
+		initialize: function () { //things that should happen when the view is initialized
+			this.render(); //renders the app view
+		},
+		render: function () {
+			this.$el.html("<h1>Stir it up.</h1>");
+			return this; //allows for future chaining
+		}
+	});
+
+	var app = new AppView();
+	window.app = app; //allows access to the app from the console once we run this.
+});
